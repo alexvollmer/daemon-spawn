a08c387929ebf85eef3291d94735f1b96acf8f82
Alex Vollmer <alex.vollmer@gmail.com>
2010-02-13 13:20:49 -0800

    Added tests for 8b4c0dc1090971785fee01b4dc3a7e47f12276a8

--- test/daemon_spawn_test.rb
+++ test/daemon_spawn_test.rb
@@ -5,20 +5,27 @@ class DaemonSpawnTest < Test::Unit::TestCase
 
   SERVERS = File.join(File.dirname(__FILE__), "servers")
 
+  def with_socket
+    socket = TCPSocket.new('localhost', 5150)
+    socket.setsockopt(Socket::SOL_SOCKET,
+                      Socket::SO_RCVTIMEO,
+                      [1, 0].pack("l_2"))
+
+    begin
+      yield(socket) if block_given?
+    ensure
+      socket.close
+    end
+  end
+
   def while_running
     Dir.chdir(SERVERS) do
       `./echo_server.rb stop`
       assert_match(/EchoServer started./, `./echo_server.rb start 5150`)
       sleep 1
-      socket = TCPSocket.new('localhost', 5150)
-      socket.setsockopt(Socket::SOL_SOCKET,
-                        Socket::SO_RCVTIMEO,
-                        [1, 0].pack("l_2"))
-
       begin
-        yield(socket) if block_given?
+        with_socket
       ensure
-        socket.close
         assert_match(//, `./echo_server.rb stop`)
         assert_raises(Errno::ECONNREFUSED) { TCPSocket.new('localhost', 5150) }
       end
@@ -59,4 +66,32 @@ class DaemonSpawnTest < Test::Unit::TestCase
     end
   end
 
+  def test_restart_after_stopped
+    Dir.chdir(SERVERS) do
+      assert_match(/EchoServer started/, `./echo_server.rb restart 5150`)
+      assert_equal(0, $?.exitstatus)
+      sleep 1
+      with_socket do |socket|
+        socket << "foobar\n"
+        assert_equal "foobar\n", socket.readline
+      end
+    end
+  end
+
+  def test_restart_after_started
+    Dir.chdir(SERVERS) do
+      assert_match(/EchoServer started/, `./echo_server.rb start 5150`)
+      assert_equal(0, $?.exitstatus)
+      sleep 1
+
+      assert_match(/EchoServer started/, `./echo_server.rb restart 5150`)
+      assert_equal(0, $?.exitstatus)
+      sleep 1
+
+      with_socket do |socket|
+        socket << "foobar\n"
+        assert_equal "foobar\n", socket.readline
+      end
+    end
+  end
 end
